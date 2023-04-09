b42fa3e10f71122fce50e992e2f3bbbda051c1b7
hillaryoyaro <hillaryoyaroh@gmail.com>
2023-04-09 10:11:12 +0300

    Data.js

---
+++ client/src/utils/Data.js
@@ -0,0 +1,28 @@
+export const services = [
+    {
+        title:"Free Shipping",
+        tagline:"From orders orders over ksh.3000",
+        image:"images/service.png",
+    },    
+ 
+    {
+        title:"Daily Surprise Offer",
+        tagline:"Save upto 25% Off",
+        image:"images/service-02.png",
+    },
+    {
+        title:"e-Mali Surport 24/7",
+        tagline:"Shop with an expert",
+        image:"images/service-03.png",
+    },
+    {
+        title:"Affordable prices",
+        tagline:"Get Our Exclusive Offer Prices.",
+        image:"images/service-04.png",
+    },
+    {
+        title:"Secure Payment",
+        tagline:"100% Protected Payment.Through e-Mali Pay and other Payment.",
+        image:"images/service-05.png",
+    },
+];
